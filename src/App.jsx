7ccb15efe33efc549cf43a33a0ce273ab9d88c2e
Mattéo Lafrancesca <matteo.lafrancesca@gmail.com>
2025-03-30 14:27:56 +0200

    deploy 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
 import Projets from "./pages/Projets";
 import Competences from "./pages/Competences";
