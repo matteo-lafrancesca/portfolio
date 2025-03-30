40ad524387550fced8fee5f0f7121d07e32e57d2
Mattéo Lafrancesca <matteo.lafrancesca@gmail.com>
2025-03-30 14:25:27 +0200

    deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="node" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/portfolio/", 
-  plugins: [react()],
-});
+  plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
+})
